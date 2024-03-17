9203613046fa4f10a122c126a1df1c7c1429277c
Amani Bobo <amanibobo1@gmail.com>
2024-03-17 17:43:29 -0400

    added sidebar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
+import SideBar from "@/components/SideBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      hello
+    <div className="md:pl-[19rem] pt-12">
+      Hello
+    </div>
+      
+      <div className="hidden md:flex h-full w-60 flex-col fixed inset-y-0 z-40">
+          <SideBar 
+          />
+        </div>
     </>
   );
 }
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,19 @@
+export default function SideBar() {
+
+  return (
+    <>
+          <div className="bg-gray-100 h-full border-r flex flex-col overscroll-y-auto shadow-sm">
+            <div className="px-6 py-10 border-b bg-white">
+                Nav
+            </div>
+            <div className="px-6 py-10 border-b bg-white">
+                Work
+            </div>
+            <div className="px-6 py-10 border-b bg-white">
+                Contact
+            </div>
+            
+          </div>
+    </>
+  );
+}
